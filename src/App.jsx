80a898c7b7e89dbff7b8663614d1d2cf491664aa
AlexisGR117 <jefer12543@gmail.com>
2024-05-23 21:50:51 -0500

    Página de materiales para admins y users

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {  BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import AdminPage from './pages/AdminPage';
+import ClientPage from './pages/ClientPage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="app">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Cliente</Link>
+            </li>
+            <li>
+              <Link to="/admin">Administrador</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<ClientPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;

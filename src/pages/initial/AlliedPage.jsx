3e268eebefecfe4a291ef5da1cfea4e9fda05597
Andrea <andrea.duran@mail.escuelaing.edu.co>
2024-05-25 21:43:53 -0500

    cambio importacion img

--- src/pages/initial/AlliedPage.jsx
+++ src/pages/initial/AlliedPage.jsx
@@ -2,13 +2,13 @@ import { Box, VStack, Image, Heading } from "@chakra-ui/react";
 import CompaniesList from "../../components/initial/CompaniesList";
 import Header from "../../components/initial/Header";
 import Contact from "../../components/initial/Contact";
-
+import allied from "../../assets/company.jpg";
 const AlliedPage = () => {
     return (
         <Box width="100vw" height="100vh" display="flex" flexDirection="column">
             <Header />
             <Box width='100%' mb="10" height='30vh' display='flex' justifyContent='center' alignItems='center' position='relative'>
-                <Image width='100%' height='100%' objectFit='cover' opacity={0.6} src="src\assets\company.jpg" alt="Plasticos" />
+                <Image width='100%' height='100%' objectFit='cover' opacity={0.6} src={allied} alt="Plasticos" />
                 <VStack position='absolute' color='gray.700' spacing={4}>
                     <Heading as='h1' size='xl' mb={4}>¡Únete a nosotros y transforma el plástico en herramientas innovadoras!</Heading>
                 </VStack>
